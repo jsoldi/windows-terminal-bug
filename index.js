1c8b6308f089aaf9343bb738eca58d264417882b
jsoldi <jsoldi@hotmail.com>
2023-07-14 16:14:56 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const port = process.argv[2] || 3000;
+const app = express();
+app.get('/', (req, res) => res.send(`Helloooo from port ${port}`));
+app.listen(port, () => console.log('Server is running at http://localhost:' + port));
+//# sourceMappingURL=index.js.map
